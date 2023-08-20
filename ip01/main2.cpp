72c142fbd0f963e9d8c33fb5fe9decb484f88ea9
Willian <92097335+Will448@users.noreply.github.com>
2023-08-20 11:54:10 -0300

    main2.cpp

--- ip01/main2.cpp
+++ ip01/main2.cpp
@@ -1,3 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout<<"Hello World\n";
+
 //ATV-13
 /*
 int valor, aux;
@@ -12,3 +19,22 @@ for ( contador = 1; contador <= 10; contador++){
 printf(" A tabuada do valor: %d * %d = %d\n", valor, contador, aux);
 }
 */
+//ATV-15
+/*
+int valor; 
+int fat = 1;
+int contador = 1; 
+
+printf("Digite um numero: ");
+scanf("%d", &valor);
+
+while ( contador <= valor ) {
+    fat *= contador;
+    contador ++;
+    printf("O fatorial: %d -- %d\n", contador, fat);
+} 
+printf("O fatorial do valor e: %d\n", fat);
+*/
+
+    return 0;
+}
