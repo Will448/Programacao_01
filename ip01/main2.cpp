8488a49562fc18a9d1ec8b0bf4635f0d24aa7301
Willian <92097335+Will448@users.noreply.github.com>
2023-08-20 11:15:39 -0300

    main2.cpp

---
+++ ip01/main2.cpp
@@ -0,0 +1,14 @@
+//ATV-13
+/*
+int valor, aux;
+int contador = 1; 
+
+printf("Digite um numero: ");
+scanf("%d", &valor);
+
+for ( contador = 1; contador <= 10; contador++){
+    aux = valor * contador;
+ // outra forma de fazer: printf("Os resultados: %d\n", valor * contador);
+printf(" A tabuada do valor: %d * %d = %d\n", valor, contador, aux);
+}
+*/
